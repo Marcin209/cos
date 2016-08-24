bdaa3373f09a5f372bed5a4f5c25f523f59d1c98
Marcin209 <marcin.sobczak209@gmail.com>
2016-08-24 12:45:32 +0200

    Poprawiona wersja map

--- Maps/lib/marker.js
+++ Maps/lib/marker.js
@@ -4,77 +4,58 @@
 
 
 function initMap() {
+   //Tablica przechowujaca markery
     var markers = [];
+    // Nazwy markerow w postaci liter z alfabetu Opcja
+    // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // index nadaje label markerom
+    var index = 0;
 
-    var myLatlng = {lat: 51.1032725, lng: 17.0284586999};
+    // zmienna przechowywujaca wspolrzedene punktu do ktorego zoomujemy mape
+    var myLatlng = {lat:  51.103316  , lng: 17.028387 };
 
-
-
+    // Tu tworzymy nasza mape
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 18,
         center: myLatlng,
-        disableDefaultUI: false
+        disableDefaultUI: false,
+        marker: markers
     });
 
-    map.event.addListener(map, 'click', function (event) {
-        addMarker(event.latLng);
-    });
-    addMarker(myLatlng);
-
-
-    function addMarker(location) {
+    function addMarkers(){
         var marker = new google.maps.Marker({
-            position: location,
-            map: map
+            position: {lat:  51.103316 + index*0.000020 , lng: 17.028387 +index*0.000020},
+            label: index.toString(),
+            map: map,
+            draggable: true,
+            title: 'Click to zoom'
+        });
+        var marker2 = new google.maps.Marker({
+            position: {lat: 51.103313 +(index)*0.000020 , lng: 17.028405 +(index)*0.000020},
+            label: (index++).toString(),
+            map: map,
+            title: 'Click to zoom',
+            draggable: true
         });
         markers.push(marker);
+        markers.push(marker2);
+
+    }
+    function calcDistance(p1, p2) {
+        return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2)).toFixed(2);
     }
 
-    function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-        }
-    }
+    document.getElementById("add").onclick = function() {
+        addMarkers();
+    };
+    
+    document.getElementById("info").onclick = function() {
+        document.getElementById("w1").innerHTML=markers[0].position;
+        document.getElementById("w2").innerHTML=markers[1].position;
+        document.getElementById("od").innerHTML=calcDistance(markers[0].position,markers[1].position);
 
+    };
 
-    function clearMarkers() {
-        setMapOnAll(null);
-    }
-
-
-    function showMarkers() {
-        setMapOnAll(map);
-    }
-
-
-    function deleteMarkers() {
-        clearMarkers();
-        markers = [];
-    }
+   
 
 }
-
-
-
-
-    // var marker = new google.maps.Marker({
-    //     position: myLatlng,
-    //     map: map,
-    //     title: 'Click to zoom'
-    // });
-    // var marker2 = new google.maps.Marker({
-    //     position: myLatlng2,
-    //     map: map,
-    //     title: 'Click to zoom'
-    // });
-    //
-    //
-    //
-    // marker.addListener('click', function() {
-    //     map.setZoom(19);
-    //     map.setCenter(marker.getPosition());
-    // });
-    // marker2.addListener('click', function() {
-    //     map.setZoom(19);
-    //     map.setCenter(marker2.getPosition());
-    // });
--- Maps/lib/query.js
+++ Maps/lib/query.js
@@ -1,8 +1,46 @@
 /**
  * Created by msobczak on 8/23/2016.
  */
-$(function() {
-    $("#map").goMap();
-
-
-});
+// $(function() {
+//
+//
+//     var mapa = $("#map").goMap({
+//         latitude: 51.000000,
+//         longitude: 17.000000,
+//         zoom: 19,
+//         maptype: 'ROADMAP',
+//         navigationControl: false,
+//         mapTypeControl: false,
+//         scrollwheel: true,
+//         disableDoubleClickZoom: true,
+//         markers: [{
+//             latitude: 51.103316,
+//             longitude: 17.028387,
+//             title: 'marker title 1',
+//             id: 'marker1',
+//             draggable: false,
+//
+//         },
+//             {
+//                 latitude: 51.103309,
+//                 longitude: 17.028414,
+//                 title: 'marker title 2',
+//                 id: 'marker2',
+//                 draggable: false
+//             }
+//         ]
+//     });
+//
+//     var p1 = new google.maps.LatLng(mapa.markers[0].latitude, mapa.markers[0].longitude);
+//     var p2 = new google.maps.LatLng(mapa.markers[1].latitude,mapa.markers[1].longitude);
+//
+//     function calcDistance(p1, p2) {
+//         return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2)).toFixed(2);
+//     }
+//
+//     $("#info").click(function(){
+//         alert(calcDistance(p1, p2));
+//
+//     });
+//
+// });
